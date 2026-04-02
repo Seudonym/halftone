8342670a74398a44befa96cb0f9598a242524583
Seudonym <wk170179@gmail.com>
2026-04-02 12:32:27 +0530

    fix: consistency, update styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,11 +5,14 @@ export const Button = ({
   children: React.ReactNode;
   onClick?: () => void;
 }) => (
-  <button onClick={onClick} className="group relative inline-block ">
+  <button
+    onClick={onClick}
+    className="group relative inline-block cursor-pointer"
+  >
     <div className="absolute inset-0 translate-x-1 translate-y-1 bg-black border-2 border-black" />
 
-    <div className="relative border-2 border-black p-2 dithered-fill flex justify-between items-center select-none bg-white group-active:translate-x-1 group-active:translate-y-1">
-      <span className="bg-white py-1 px-2 border border-black text-xs text-black font-mono tracking-widest">
+    <div className="relative border-2 border-black p-1 dithered-fill flex justify-between items-center select-none bg-white group-active:translate-x-1 group-active:translate-y-1">
+      <span className="bg-white px-2 border border-black text-xs text-black font-mono tracking-widest">
         {children}
       </span>
     </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,14 +9,20 @@ function App() {
     <main className="h-screen bg-white text-black">
       <div className="relative flex flex-col h-full items-center justify-center overflow-hidden border-2 border-black bg-white">
         {/* Header */}
-        <div className="dithered-fill h-24 px-4 sm:px-8 flex items-center w-full border-b-2 border-black">
+        <div className="dithered-fill h-24 px-4 sm:px-8 flex justify-between items-center w-full border-b-2 border-black">
           <Badge>-&gt; Work In Progress</Badge>
+          <div className="space-x-4">
+            <a href="https://github.com/seudonym/halftone" target="_blank">
+              <Button>GitHub</Button>
+            </a>
+            <Button>Docs</Button>
+          </div>
         </div>
 
         <section className="relative z-10 flex max-w-6xl h-full flex-col items-center justify-around px-4 text-center">
           <Button>HALFTONE UI</Button>
 
-          <h1 className="font-mono text-9xl font-black uppercase leading-25 tracking-tight">
+          <h1 className="font-mono text-9xl font-black uppercase tracking-tight">
             Black
             <br />
             White
